4c2d0718f9554c1aa0c9a02f294bb609259a1c55
Michael L Heuer <heuermh@acm.org>
2012-10-10 13:13:41 -0500

    adding pom.xml, client module

---
+++ client/src/main/java/com/github/heuermh/personalgenome/ProfileName.java
@@ -0,0 +1,59 @@
+/*
+
+    personal-genome-client  Java client for the 23andMe Personal Genome API.
+    Copyright (c) 2012 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package com.github.heuermh.personalgenome.client;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * Profile name.
+ */
+@Immutable
+public final class ProfileName {
+    private final String id;
+    private final String firstName;
+    private final String lastName;
+
+    public ProfileName(final String id, final String firstName, final String lastName) {
+        checkNotNull(id);
+        checkNotNull(firstName);
+        checkNotNull(lastName);
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
