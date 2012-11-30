92d8b4e1d9ed172cdf012f2bda8a493c7be13efd
Michael Heuer <heuermh@acm.org>
2012-11-30 11:33:18 -0600

    adding grant_type parameter, temporary debugging output

--- client/src/main/java/com/github/heuermh/personalgenome/client/scribe/PersonalGenomeApi.java
+++ client/src/main/java/com/github/heuermh/personalgenome/client/scribe/PersonalGenomeApi.java
@@ -25,15 +25,23 @@ package com.github.heuermh.personalgenome.client.scribe;
 
 import org.scribe.builder.api.DefaultApi20;
 import org.scribe.model.OAuthConfig;
+import org.scribe.model.OAuthConstants;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
 import org.scribe.model.Verb;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+import org.scribe.oauth.OAuth20ServiceImpl;
 import org.scribe.utils.OAuthEncoder;
 
 /**
  * Personal genome API.
  */
 public final class PersonalGenomeApi extends DefaultApi20 {
-    private static final String ACCESS_TOKEN_URL = "https://api.23andme.com/token/?grant_type=authorization_code";
-    private static final String AUTHORIZE_URL = "https://api.23andme.com/authorize/?client_id=%s&response_type=code&redirect_uri=%s&scope=%s";
+    //private static final String ACCESS_TOKEN_URL = "https://api.23andme.com/token?grant_type=authorization_code";
+    private static final String ACCESS_TOKEN_URL = "https://api.23andme.com/token";
+    private static final String AUTHORIZE_URL = "https://api.23andme.com/authorize?client_id=%s&response_type=code&redirect_uri=%s&scope=%s";
 
     @Override
     public String getAccessTokenEndpoint() {
@@ -48,5 +56,31 @@ public final class PersonalGenomeApi extends DefaultApi20 {
     @Override
     public String getAuthorizationUrl(final OAuthConfig config) {
         return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()), config.getScope());
+    }
+
+    @Override
+    public OAuthService createService(final OAuthConfig config) {
+        return new OAuth20ServiceImpl(this, config) {
+            @Override
+            public Token getAccessToken(final Token requestToken, final Verifier verifier)
+            {
+                OAuthRequest request = new OAuthRequest(PersonalGenomeApi.this.getAccessTokenVerb(), PersonalGenomeApi.this.getAccessTokenEndpoint());
+                request.addQuerystringParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
+                request.addQuerystringParameter(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
+                request.addQuerystringParameter("grant_type", "authorization_code"); // todo:  move this to OAuth20ServiceImpl.java ?
+                request.addQuerystringParameter(OAuthConstants.CODE, verifier.getValue());
+                request.addQuerystringParameter(OAuthConstants.REDIRECT_URI, config.getCallback());
+                if(config.hasScope()) request.addQuerystringParameter(OAuthConstants.SCOPE, config.getScope());
+
+                System.out.println("request=" + request.toString());
+                System.out.println("headers=" + request.getHeaders().toString());
+                System.out.println("request queryStringParameters=" + request.getQueryStringParams().asOauthBaseString());
+                System.out.println("request queryStringParameters=" + request.getQueryStringParams().asFormUrlEncodedString());
+                System.out.println("request oauthParameters=" + request.getOauthParameters().toString());
+
+                Response response = request.send();
+                return PersonalGenomeApi.this.getAccessTokenExtractor().extract(response.getBody());
+            }
+        };
     }
 }
